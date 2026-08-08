ff7ce3f1e48dc2a63763ee314803f3612dd8d4f9
rahulbormon999-star <rahulbormon999@gmail.com>
2026-08-08 17:01:31 +0600

    Delete api/auth/logout.js

--- api/auth/logout.js
+++
@@ -1,6 +0,0 @@
-import { clearSessionCookie } from '../../lib/auth.js';
-
-export default async function handler(req, res) {
-  clearSessionCookie(res);
-  return res.status(200).json({ success: true });
-}
