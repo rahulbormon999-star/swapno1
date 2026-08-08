0e86f4dddb8bd325e4dc8de50982fd4fbe04c2e3
rahulbormon999-star <rahulbormon999@gmail.com>
2026-08-08 17:01:45 +0600

    Delete api/admin/logout.js

--- api/admin/logout.js
+++
@@ -1,6 +0,0 @@
-import { clearAdminSessionCookie } from '../../lib/auth.js';
-
-export default async function handler(req, res) {
-  clearAdminSessionCookie(res);
-  return res.status(200).json({ success: true });
-}
